7ab08ec387f55ce8139cf8a0b799570ead9a70b4
lipengzhou <lpzmail@163.com>
2020-12-03 20:52:34 +0800

    init

---
+++ pages/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1>Hello world!</h1>
+    <ul>
+      <li
+        v-for="post in posts"
+        :key="post.id"
+      >{{ post.title }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  async asyncData () {
+    const { data } = await axios.get('https://jsonplaceholder.typicode.com/posts')
+    return {
+      posts: data
+    }
+  }
+}
+</script>
